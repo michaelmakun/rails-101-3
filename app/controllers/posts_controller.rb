de344cac35106c5728485836916a2c2059e2a6c4
makun <michaelmakun@aliyun.com>
2016-12-31 23:07:59 +0800

    implement my post & 'edit' 'delete'

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,13 @@
 class PostsController < ApplicationController
 
   before_filter :authenticate_user!, :only => [:new, :create]
-
+  before_filter :set_GroupPost_and_check_permission, :only => [:edit, :update, :destroy]
   def new
     @group = Group.find(params[:group_id])
     @post = Post.new
+  end
+
+  def edit
   end
 
   def create
@@ -20,7 +23,28 @@ class PostsController < ApplicationController
     end
   end
 
+  def update
+    if @post.update(post_params)
+      redirect_to account_posts_path , notice: 'Post Update Success.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to account_posts_path, alert:'Post deleted'
+  end
+
   private
+
+  def set_GroupPost_and_check_permission
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    if current_user != @post.user
+      redirect_to root_path, alert:'You have no permission.'
+    end
+  end
 
   def post_params
     params.require(:post).permit(:content)
